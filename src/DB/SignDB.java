f817ae7a3c5612280acc709d74436dc9539fdd9b
sphist94 <sphist94@DESKTOP-UA80432>
2019-11-24 17:05:04 +0900

    SignIn, SignUp Implementation with DB

---
+++ src/DB/SignDB.java
@@ -0,0 +1,78 @@
+package DB;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SignDB {
+	static ResultSet rs;
+
+	public static boolean DidExistId(String id) {
+		try {
+			DBConnection.stmt = DBConnection.conn.createStatement();
+			String sql = "select Id from Account" + " where Id=" + "'" + id + "'";
+			ResultSet rs = DBConnection.stmt.executeQuery(sql);
+			// 회수해온 아이디 비밀번호가 전부 동일한 경우
+			if (rs.next() && id.equals(rs.getString(1)))
+				return true;
+			return false;
+		} catch (SQLException ex) {
+			System.err.println("SQL error = " + ex.getMessage());
+			return false;
+		}
+	}
+
+	public static int login(String id, String password) {
+		try {
+			DBConnection.stmt = DBConnection.conn.createStatement();
+			String sql = "select Id, Password, Account_type from Account" + " where Id=" + "'" + id + "'";
+			ResultSet rs = DBConnection.stmt.executeQuery(sql);
+			// 회수해온 아이디 비밀번호가 전부 동일한 경우
+			if (rs.next() && id.equals(rs.getString(1)) && password.equals(rs.getString(2)))
+				return Integer.parseInt(rs.getString(3));
+			return 0;
+		} catch (SQLException ex2) {
+			System.err.println("sql error = " + ex2.getMessage());
+			return 0;
+		}
+	}
+
+	public static void signUp(String[] input, String account_type) {
+		try {
+			DBConnection.stmt = DBConnection.conn.createStatement();
+			StringBuffer sb = new StringBuffer();
+			sb.append("insert into ACCOUNT (Id, Password, Lname, Fname, Phone_no, Birth_date, Gender, Email, Address, Occupation, Account_type) values (");
+			for (int i = 0; i < input.length; ++i) {
+				if (input[i].isEmpty())
+					sb.append("NULL, ");
+				else
+					sb.append("'" + input[i] + "', ");
+			}
+			sb.append(account_type + ")");
+			String sql = sb.toString();
+			DBConnection.stmt.executeUpdate(sql);
+			
+			//account_type에 따른 구매자, 판매자, 관리자 계정 업데이트
+			switch(Integer.parseInt(account_type)) {
+			case 1:
+				sb = new StringBuffer();
+				sb.append("insert into customer(Cuid) values ("+input[0]+")");
+				DBConnection.stmt.executeUpdate(sb.toString());
+				break;
+			case 2:
+				sb = new StringBuffer();
+				sb.append("insert into dealer(Cuid) values ("+input[0]+")");
+				DBConnection.stmt.executeUpdate(sb.toString());
+				break;
+			case 3:
+				sb = new StringBuffer();
+				sb.append("insert into administrator(Cuid) values ("+input[0]+")");
+				DBConnection.stmt.executeUpdate(sb.toString());
+				break;
+			}
+			
+		} catch (SQLException ex2) {
+			System.err.println("sql error = " + ex2.getMessage());
+			System.exit(1);
+		}
+	}
+}
--- src/View/SignView.java
+++ src/View/SignView.java
@@ -1,11 +1,16 @@
 package View;
 
+import Functions.CheckConditions;
 import Functions.SignInputType;
+
+import java.util.Arrays;
+
+import DB.SignDB;
 
 public class SignView extends BasicView {
 	public void loadSignIn() {
 		boolean isSignInSuccess = false;
-
+		int account_type = 0;
 		// 로그인 실패시 계속해서 이 페이지에 머물면서 로그인을 시도
 		while (!isSignInSuccess) {
 			printPageStart();
@@ -19,19 +24,21 @@ public class SignView extends BasicView {
 			String password = getInput(SignInputType.PW, "PW: ");
 			printPageEnd();
 
-			// TODO:
-			// 1. 입력 ID를 DB의 계정튜플들과 비교해 존재하는지 비교
-			// 2. 존재한다면 비밀번호 일치하는 비교하고, 일치하면 계정 유형(구매자, 판매자, 관리자)에 따라 페이지 로딩
-			// 3. 입력 ID가 존재하지 않거나 비밀번호가 일치하지 않는다면 에러 메시지 출력하고 해당 페이지 리로딩
-			// if(){
-			{
+			// SignDB.Login에 ID와 PW를 넘겨주면
+			// DB와 비교를 해서 있는 정보면 account_type을 반환해준다.
+			// 만약 account_type이 0이면 없는 계정, 1: 구매자, 2: 판매자, 3: 관리자
+			if ((account_type = SignDB.login(id, password)) != 0) {
 				isSignInSuccess = true;
+			} else {
+				printPageMiddle();
+				System.out.println("존재하지 않는 ID이거나 틀린 비밀번호입니다.");
+				System.out.println("다시 입력해주십시오.");
+				printPageMiddle();
 			}
 		}
 
+		// account_type에 따른 뷰 호출
 		if (isSignInSuccess) {
-			// int account_type = getAccountType(id);
-			int account_type = 1;
 			switch (account_type) {
 			case 1:
 				CustomerView cv = new CustomerView();
@@ -49,34 +56,100 @@ public class SignView extends BasicView {
 		}
 	}
 
-	public void loadSignUp() {
-		while (true) {
-			printPageStart();
-			System.out.println("회원가입 페이지입니다.");
-			
-			if (printBack())
-				break;
-			
-			System.out.println("가입하시고자 하는 유형을 입력해주십시오.");
-			String account_type = getInput(SignInputType.ACCOUNT_TYPE, "1. 구매자   2. 판매자 ");
-			System.out.println("해당하는 사항에 알맞게 기입해주십시오. (*는 필수 정보)");
-			System.out.println("입력하지 않을 시 - 키를 입력해주십시오.");
-			String id = getInput(SignInputType.ID, "*ID: ");
-			String pw = getInput(SignInputType.PW, "*PW: ");
-			String last_name = getInput(SignInputType.LNAME, "*Lname: ");
-			String first_name = getInput(SignInputType.FNAME, "*Fname: ");
-			String phone = getInput(SignInputType.PHONE, "*Phone(NNN-NNNN-NNNN): ");
-			String birthdate = getInput(SignInputType.BIRTHDATE, "Birthdate(YYYY-MM-DD): ");
-			String gender = getInput(SignInputType.GENDER, "Gender: (M/F) ");
-			String email = getInput(SignInputType.EMAIL, "Email: ");
-			String address = getInput(SignInputType.ADDRESS, "Address: ");
-			String occupation = getInput(SignInputType.OCCUPATION, "Occupation: ");
-			printPageEnd();
-
-			// TODO:
-			// 1. ID 입력시 DB의 데이터와 비교해 이미 존재하는 아이디인지 확인이 필요
-			// 2. Phone, Gender, Birthdate의 입력 값이 ()와 일치하지 않으면 다시 수행
-			// 3. 회원가입이 성공했으면 성공 메시지를 출력
+	private String FillSignUpInfo(int select) {
+		switch (select) {
+		case 1:
+			return getInput(SignInputType.ID, "*ID: ");
+		case 2:
+			return getInput(SignInputType.PW, "*PW: ");
+		case 3:
+			return getInput(SignInputType.LNAME, "*Lname: ");
+		case 4:
+			return getInput(SignInputType.FNAME, "*Fname: ");
+		case 5:
+			return getInput(SignInputType.PHONE, "*Phone(NNN-NNNN-NNNN): ");
+		case 6:
+			return getInput(SignInputType.BIRTHDATE, "Birthdate(YYYY-MM-DD): ");
+		case 7:
+			return getInput(SignInputType.GENDER, "Gender: (M/F) ");
+		case 8:
+			return getInput(SignInputType.EMAIL, "Email: ");
+		case 9:
+			return getInput(SignInputType.ADDRESS, "Address: ");
+		case 10:
+			return getInput(SignInputType.OCCUPATION, "Occupation: ");
+		default:
+			return "";
 		}
+	}
+
+	private boolean didFillRequiredInfo(String[] input) {
+		for (int i = 0; i < 5; ++i) {
+			if (input[i].isEmpty())
+				return false;
+		}
+		return true;
+	}
+
+	public void loadSignUp() {
+		printPageStart();
+		System.out.println("회원가입 페이지입니다.");
+
+		if (printBack())
+			return;
+
+		System.out.println("가입하시고자 하는 유형을 입력해주십시오.");
+		String account_type = getInput(SignInputType.ACCOUNT_TYPE, "1.구매자   2.판매자 \n");
+		String[] input = new String[10];
+		Arrays.fill(input, "");
+		while (true) {
+			printPageMiddle();
+			System.out.println("입력하고자 하는 사항을 선택해주십시오.");
+			System.out.println("1.아이디*  2.비밀번호*  3.성*  4.이름*  5.휴대전화 번호*  6.생년월일  7.성별  8.이메일  9.주소  10.직업");
+			System.out.println("해당하는 사항에 알맞게 기입해주십시오. (*는 필수 정보)");
+			System.out.println("완료를 원하시면 11를, 종료를 원하시면 12를 입력해주세요");
+			System.out.println("그 외의 숫자나 문자는 무시됩니다.");
+			printPageMiddle();
+
+			String selection = sc.nextLine();
+			if (CheckConditions.isInteger(selection)) {
+				int select = Integer.parseInt(selection);
+				// select가 정수이지만 사항과 관련없는 정수일경우 다시 시작
+				if (select < 1 || select > 12)
+					continue;
+				// 11은 입력완료를 의미. 따라서 필수정보를 다 기입했는지 확인이 필요함
+				else if (select == 11) {
+					// 필수정보를 전부 입력한 경우
+					if (didFillRequiredInfo(input)) {
+						// DB를 업데이트 해준다
+						SignDB.signUp(input, account_type);
+						break;
+					}
+					// 필수정보를 전부 입력하지 않은 경우
+					else {
+						printPageMiddle();
+						System.out.println("아직 필수정보들을 전부 입력하지 않았습니다. 다시 확인해주세요.");
+						System.out.println("회원가입이 완료되었습니다.");
+						printPageMiddle();
+						continue;
+					}
+				}
+				// 12는 종료를 의미, 따라서 회원정보 페이지 종료
+				else if (select == 12) {
+					break;
+				}
+
+				input[select - 1] = FillSignUpInfo(select);
+
+				// ID는 이미 존재하는건지 확인이 필요
+				while (select == 1 && SignDB.DidExistId(input[select - 1])) {
+					printPageMiddle();
+					System.out.println("이미 존재하는 ID입니다. 다시 입력해주십시오.");
+					input[select - 1] = FillSignUpInfo(select);
+					printPageMiddle();
+				}
+			}
+		}
+		printPageEnd();
 	}
 }
