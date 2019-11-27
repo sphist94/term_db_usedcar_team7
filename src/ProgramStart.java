0cf9a1edff10085ddf601c14eff30c7b8e0c9431
sphist94 <sphist94@DESKTOP-UA80432>
2019-11-27 13:22:50 +0900

    up

--- src/ProgramStart.java
+++ src/ProgramStart.java
@@ -25,11 +25,11 @@ public class ProgramStart {
 		
 		
 		//디버그나 테스트할때는 주석처리 해둘것
-//		System.out.println("\n데이터 초기화 중입니다. 잠시만 기다려 주십시오.");
-//		InsertionPhase2Sqls.readSqlFiles("Team8_phase1.sql");
-//		System.out.println("데이터 입력중입니다. 잠시만 기다려 주십시오.");
-//		InsertionPhase2Sqls.readSqlFiles("Team8_phase2.sql");
-//		System.out.println("기다려주셔서 감사합니다.");
+		System.out.println("\n데이터 초기화 중입니다. 잠시만 기다려 주십시오.");
+		InsertionPhase2Sqls.readSqlFiles("Team8_phase1.sql");
+		System.out.println("데이터 입력중입니다. 잠시만 기다려 주십시오.");
+		InsertionPhase2Sqls.readSqlFiles("Team8_phase2.sql");
+		System.out.println("기다려주셔서 감사합니다.");
 		//주석처리 여기까지
 		
 		MainView mv = new MainView();
